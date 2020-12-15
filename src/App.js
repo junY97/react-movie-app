7622126f5d3f6cf7cd6e6fa193030ecc4f7e68fa
junyeong-is-developer <whwnsdud2024@naver.com>
2020-12-15 20:06:16 +0900

    12-15

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+
+
+class App extends Component{
+  state ={
+      isLoading:true,
+      movies:[],
+  };
+  componentDidMount(){
+    //영화 데이터 로딩
+    setTimeout(()=>{
+      this.setState({isLoading:false});
+    },6000);
+  }
+  
+  render(){  
+    const { isLoading } = this.state;
+
+    return <div>{isLoading ? 'Lodaing...'  : 'We are ready'} </div>;
+  }  
 }
 
 export default App;
