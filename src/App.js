9a2a9ebc0008711e226e12d5305589c12ce2bb95
junyeong-is-developer <whwnsdud2024@naver.com>
2020-12-16 16:57:36 +0900

    12-16 16:57

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
 import './App.css';
 import React, { Component } from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+
 
 
 class App extends Component{
@@ -8,17 +11,36 @@ class App extends Component{
       isLoading:true,
       movies:[],
   };
+  getmovies = async () =>{
+    const {
+      data:{
+        data:{movies},
+      },
+    }=await axios.get('https://yts-proxy.now.sh/list_movies.json');
+  this.setState({movies:movies,isLoading:false});
+  };
+
   componentDidMount(){
     //영화 데이터 로딩
-    setTimeout(()=>{
-      this.setState({isLoading:false});
-    },6000);
+   this.getmovies(); 
+   
   }
-  
-  render(){  
-    const { isLoading } = this.state;
-
-    return <div>{isLoading ? 'Lodaing...'  : 'We are ready'} </div>;
+   render(){
+    const { isLoading,movies} = this.state;
+    return( <div>{isLoading ? 'Lodaing...'  : movies.map(movie=>{
+      console.log(movie);
+     return(
+      <Movie 
+      id={movie.id} 
+      year={movie.year}
+      title={movie.title}
+      summary={movie.summary}
+      poster={movie.medium_cover_image}
+      />
+      );
+    })} 
+    </div>
+    );
   }  
 }
 
