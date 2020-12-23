49e50d7995e86a478ee69097141c015d2da6f0fe
junyeong-is-developer <whwnsdud2024@naver.com>
2020-12-23 14:48:54 +0900

    12-23

--- src/App.js
+++ src/App.js
@@ -1,59 +1,13 @@
 
 import './App.css';
-import React, { Component } from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import React from 'react';
+import Home from './routes/Home';
 import './App.css';
 
 
 
-class App extends Component{
-  state ={
-      isLoading:true,
-      movies:[],
-  };
-  getmovies = async () =>{
-    const {
-      data:{
-        data:{movies},
-      },
-    }=await axios.get('https://yts-proxy.now.sh/list_movies.json');
-  this.setState({movies:movies,isLoading:false});
-  };
-
-  componentDidMount(){
-    //영화 데이터 로딩
-   this.getmovies(); 
-   
-  }
-   render(){
-    const { isLoading,movies} = this.state;
-    return( <seciton className="container">{isLoading ? (
-      <div className="loader">
-        <span className="loader__text">Loading..</span>
-
-      </div>
-      
-    
-    
-    ):(
-      <div className="movies"> 
-      {movies.map(movie=>(
-      <Movie 
-      key={movie.id}
-      id={movie.id} 
-      year={movie.year}
-      title={movie.title}
-      summary={movie.summary}
-      poster={movie.medium_cover_image}
-      genres={movie.genres}
-      />
-    ))}
-    </div> 
-    )}
-    </seciton>
-    );
-  }  
-}
+ function App() {
+   return <Home />
+ }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
+
+
+
+class Home extends Component{
+  state ={
+      isLoading:true,
+      movies:[],
+  };
+  getmovies = async () =>{
+    const {
+      data:{
+        data:{movies},
+      },
+    }=await axios.get('https://yts-proxy.now.sh/list_movies.json');
+  this.setState({movies:movies,isLoading:false});
+  };
+
+  componentDidMount(){
+    //영화 데이터 로딩
+   this.getmovies(); 
+   
+  }
+   render(){
+    const { isLoading,movies} = this.state;
+    return( 
+    <seciton className="container">
+      {isLoading ? (
+      <div className="loader">
+        <span className="loader__text">Loading..</span>
+        
+      </div>
+      
+    
+    
+    ):(
+      <div className="movies"> 
+      {movies.map(movie=>(
+      <Movie 
+      key={movie.id}
+      id={movie.id} 
+      year={movie.year}
+      title={movie.title}
+      summary={movie.summary}
+      poster={movie.medium_cover_image}
+      genres={movie.genres}
+      />
+    ))}
+    </div> 
+    )}
+    </seciton>
+    );
+  }  
+}
+
+export default Home;
